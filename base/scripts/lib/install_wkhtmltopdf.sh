ca6a383639be514f6f4f0433fcddab02dd6c14a8
hatem.jerbi <hatem.jerbi@redlean.io>
2017-02-03 05:38:07 +0100

    add libfontconfig1 libxrender1

--- base/scripts/lib/install_wkhtmltopdf.sh
+++ base/scripts/lib/install_wkhtmltopdf.sh
@@ -3,6 +3,8 @@ set -e
 WKHTMLTOX_XZ_FILE=wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
 WKHTMLTOX_TAR_FILE=wkhtmltox-0.12.3_linux-generic-amd64.tar
 
+apt-get install -y libfontconfig1 libxrender1
+
 cd /tmp
 curl -L -O http://download.gna.org/wkhtmltopdf/0.12/0.12.3/$WKHTMLTOX_XZ_FILE
 
